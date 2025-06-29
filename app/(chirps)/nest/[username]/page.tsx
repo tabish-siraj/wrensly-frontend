36975cd5e097ea6cf4fb85a188f5f3cf49ff25ba
tabish-siraj <tabish1226@gmail.com>
2025-06-29 23:03:35 +0500

    pofile

--- app/(chirps)/nest/[username]/page.tsx
+++ app/(chirps)/nest/[username]/page.tsx
@@ -11,11 +11,9 @@ interface ProfilePageProps {
 export default function ProfilePage({ params }: ProfilePageProps) {
   const { user } = useUserStore();
   const { username } = React.use(params);
-  // if (username !== user?.username) return;
   return (
     <div className="w-1/2 m-auto">
       <div className="w-full max-w-4xl mx-auto">
-        {/* Cover Image */}
         <div className="relative w-full h-[200px] bg-gray-200">
           <Image
             src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQp8_W4z4PTHC0ogzdUY3UO9t35bbtSzvxFiA&s" // Replace with your cover image
@@ -25,9 +23,7 @@ export default function ProfilePage({ params }: ProfilePageProps) {
           />
         </div>
 
-        {/* Profile + Name Section */}
         <div className="relative px-4">
-          {/* Profile Picture */}
           <div className="absolute -top-16 left-4">
             <Avatar className="w-32 h-32 border-4 border-white">
               <AvatarImage src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRlHPCQDGxzqlFNGeeP1WPx_5tLK03EMXLwpA&s" alt={username} />
@@ -35,9 +31,8 @@ export default function ProfilePage({ params }: ProfilePageProps) {
             </Avatar>
           </div>
 
-          {/* Name & Username */}
           <div className="pt-20">
-            <h1 className="text-xl font-bold">{user?.Profile?.firstName} {user?.Profile?.lastName} </h1>
+            <h1 className="text-xl font-bold">{user?.firstName} {user?.lastName} </h1>
             <p className="text-gray-500">@{username}</p>
           </div>
         </div>
