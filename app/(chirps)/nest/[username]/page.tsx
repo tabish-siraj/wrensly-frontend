f7518b0e66e3c382c930634c2214add2745ad185
tabish-siraj <tabish1226@gmail.com>
2025-07-02 00:22:32 +0500

    build

--- app/(chirps)/nest/[username]/page.tsx
+++ app/(chirps)/nest/[username]/page.tsx
@@ -1,14 +1,8 @@
 "use client";
 
 import React from "react";
-import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
-import Image from "next/image";
-import useUserStore from "@/src/stores/userStore";
 import ProfileCard from "@/components/card/NestCard";
-interface ProfilePageProps {
-  params: Promise<{ username: string }>;
-}
 
-export default function ProfilePage({ params }: ProfilePageProps) {
+export default function ProfilePage() {
   return <ProfileCard />
 }
