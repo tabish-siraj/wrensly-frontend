856a06b1e04b4f47e56fb32757daa913c7b66eb5
tabish-siraj <tabish1226@gmail.com>
2025-10-27 22:51:34 +0500

    reset password

--- hooks/user/useForgotPassword.ts
+++ hooks/user/useForgotPassword.ts
@@ -2,10 +2,20 @@ import { useMutation } from "@tanstack/react-query";
 import api from "@/lib/api";
 
 export const useForgotPassword = () => {
-
     return useMutation({
         mutationFn: async (email: { email: string }) => {
             return await api.post("/auth/forgot-password", email);
+        },
+    });
+};
+
+export const useResetPassword = () => {
+    return useMutation({
+        mutationFn: async (payload: { token: string; password: string }) => {
+            return await api.post("/auth/reset-password", payload);
+        },
+        onSuccess: () => {
+            window.location.href = "/auth/login";
         },
     });
 };
